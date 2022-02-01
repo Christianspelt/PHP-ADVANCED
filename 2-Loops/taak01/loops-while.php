39d4ddcaa96b53f921e8eac4ac9dc07e9abd367e
Christian <Christian.Spelt@student.rocva.nl>
2022-02-01 11:35:25 +0100

    master

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,12 @@
+<?php
+$leeftijd =0;
+
+while($leeftijd<=17){
+    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen. ";
+    $leeftijd++;
+
+};
+if($leeftijd =18){
+    echo "Ik ben $leeftijd jaar oud, dus ik heb stemrecht!";
+}
+?>
