657c8f5aec32bf8d8854011faaf407d82a679c39
Christian <Christian.Spelt@student.rocva.nl>
2022-02-01 11:14:04 +0100

    MASTER

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,7 @@
+<?php
+$persoon['voornaam'] ="Christian";
+$persoon['achternaam']= "spelt";
+$persoon['age']= "17";
+$persoon['opleiding']="softwaredevolper";
+echo $persoon['voornaam']." ". $persoon['achternaam'] ." is ". $persoon['age'] ."jaar oud en leerd ". $persoon['opleiding'].".";
+?>
