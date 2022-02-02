d461473fa2ad55da9c9757c73384fcfc3077b30b
Christian <Christian.Spelt@student.rocva.nl>
2022-02-02 11:49:03 +0100

    master

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["voornaam"];
+ 
+echo $_POST["achternaam"];?><br>
+je is klas: <?php echo $_POST["klas"]; ?><br>
+je leeftijd is: <?php echo $_POST["leeftijd"]; ?><br>
+je adres is: <?php echo $_POST["adres"]; ?><br>
+je woont in.: <?php echo $_POST["woonplaats"]; ?><br>
+
+
+</body>
+</html> 
