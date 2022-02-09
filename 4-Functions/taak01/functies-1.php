09c0e1bf2768a82585611cb40e11fcacda8be161
Christian <Christian.Spelt@student.rocva.nl>
2022-02-09 09:44:31 +0100

    master

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+function schrijfKleurs(){
+    echo "<p style=color:red;>Deze tekst is in de kleur red.</p>";
+    echo "<p style=color:green;>Deze tekst is in de kleur groen.</p>";
+    echo "<p style=color:blue;>Deze tekst is in de kleur blue.</p>";
+    echo "<p style=color:yellow;>Deze tekst is in de kleur geel.</p>";
+}
+
+schrijfKleurs(); 
+  
+    ?>
+</body>
+</html>
